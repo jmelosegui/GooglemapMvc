5c946b5dd37c0d0cc834bef962aabea67d99672d
jmelosegui <elosegui@gmail.com>
2014-10-16 12:57:36 -0400

    Fixes #7: Added styled map type sample

---
+++ src/Jmelosegui.Mvc.Googlemap.Examples/Controllers/MapType/StyledMapTypeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Jmelosegui.Mvc.Googlemap.Examples.Controllers
+{
+    public partial class MapTypeController
+    {
+        public ActionResult StyledMapType()
+        {
+            return View();
+        }
+    }
+}
