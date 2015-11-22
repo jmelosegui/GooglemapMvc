2ab8526f9114dbfd5a4a97a6eff628a6ab15a608
jmelosegui <elosegui@gmail.com>
2015-11-21 22:30:35 -0500

    Add automatic GitVersioning

--- src/Jmelosegui.Mvc.Googlemap.Examples/Properties/AssemblyInfo.cs
+++ src/Jmelosegui.Mvc.Googlemap.Examples/Properties/AssemblyInfo.cs
@@ -21,15 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6ea9d1f2-b509-4258-989d-5579da87a164")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
